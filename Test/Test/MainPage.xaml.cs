fbbd021189298b5319f937a27ecc7e49a3a36a71
김태오(Taeo Kim) <38588097+Sunday5214@users.noreply.github.com>
2023-03-20 20:19:30 +0900

    add. maui 추가

---
+++ Test/Test/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Test;
+
+public partial class MainPage : ContentPage
+{
+	 public ObservableCollection<string> ToolList { get; set; } = new ObservableCollection<string>()
+        {
+            "청소기",
+            "쓰레기봉투",
+            "수세미",
+            "욕실세제",
+            "밀대걸레",
+            "고무장갑",
+            "주방세제",
+            "걸레"
+        };
+
+    public MainPage()
+    {
+        InitializeComponent();
+	}
+
+
+}
+
